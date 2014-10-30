d0c1b24466c6e6cb721754010c1c8679a36e2de6
Kamil Tekiela <tekiela246@gmail.com>
2014-10-30 11:38:39 +0000

    laptop

--- Stupid SE project.cpp
+++ Stupid SE project.cpp
@@ -4,12 +4,14 @@ int main()
 {
 	TollRoadCollector* tollRoadCollector = new TollRoadCollector();
 	
+	//for debugging purposes we are fakin user interaction
 	tollRoadCollector->vehicleDetected(1, "000", 1, 1);
 	cout << endl;
 	tollRoadCollector->vehicleDetected(0, "KE00000", 1, 1);
 	cout << endl;
 	tollRoadCollector->vehicleDetected(10, "000", 1, 1);
 	cout << endl;
+
 
 	getchar();
 }
